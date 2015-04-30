b0a6ef4ba6a5840d7ee3980c9b1e4efcab138900
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-04-30 15:03:24 +0200

    [FEATURE] Support for easier to use NoCaptcha

--- class.tx_jmrecaptcha.php
+++ class.tx_jmrecaptcha.php
@@ -26,44 +26,70 @@
 /**
  * Plugin 'reCAPTCHA' for the 'jm_recaptcha' extension.
  *
- * @author	Markus Blaschke, TEQneers GmbH & Co. KG <blaschke@teqneers.de>
- * @author	Jens Mittag <jens.mittag@prime23.de>
+ * @author  Markus Blaschke, TEQneers GmbH & Co. KG <blaschke@teqneers.de>
+ * @author  Jens Mittag <jens.mittag@prime23.de>
  */
-
-class tx_jmrecaptcha extends tslib_pibase {
-	public $prefixId		= 'tx_jmrecaptcha'; // Same as class name
-	public $scriptRelPath	= 'class.tx_jmrecaptcha.php'; // Path to this script relative to the extension dir.
-	public $extKey			= 'jm_recaptcha'; // The extension key.
+class tx_jmrecaptcha extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
+	public $prefixId = 'tx_jmrecaptcha'; // Same as class name
+	public $scriptRelPath = 'class.tx_jmrecaptcha.php'; // Path to this script relative to the extension dir.
+	public $extKey = 'jm_recaptcha'; // The extension key.
 
 	public $extConf = NULL;
 
-	public function main($content, $conf) { }
+	public $conf;
+
+	/**
+	 * @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+	 */
+	public $typoscriptFrontendController;
+
+	public function main($content, $conf) {
+	}
 
 	/**
 	 * Build reCAPTCHA Frontend HTML-Code
 	 *
-	 * @param 	string	$error	Error Code (optional)
-	 * @return	string			reCAPTCHA HTML-Code
+	 * @param  string $error Error Code (optional)
+	 * @return  string      reCAPTCHA HTML-Code
 	 */
 	public function getReCaptcha($error = NULL) {
-		global $TSFE;
 		$this->pi_loadLL();
 
-		// save plugin configuration inside a local array
-		$conf = $TSFE->tmpl->setup['plugin.']['tx_jmrecaptcha.'];
+		$this->initConfiguration();
 
-		// extract server url and public key
-		if($conf['use_ssl'] || !empty($_SERVER["HTTPS"]) || $_SERVER['SERVER_PORT'] == 443) {
-			$server = rtrim($conf['api_server_secure'], '/');
-		} else {
-			$server = rtrim($conf['api_server'], '/');
+		if ($this->conf['captcha_type'] === 'nocaptcha') {
+			return $this->renderNoCaptcha();
+		} elseif ($this->conf['captcha_type'] === 'recaptcha') {
+			return $this->renderReCaptcha($error);
 		}
-		$key = $conf['public_key'];
+	}
 
+	protected function initConfiguration() {
+		$this->typoscriptFrontendController = $GLOBALS['TSFE'];
+		$this->conf = $this->typoscriptFrontendController->tmpl->setup['plugin.']['tx_jmrecaptcha.'];
+		if ($this->conf['use_ssl'] || !empty($_SERVER["HTTPS"]) || $_SERVER['SERVER_PORT'] == 443) {
+			$this->conf['server'] = rtrim($this->conf['api_server_secure'], '/');
+		} else {
+			$this->conf['server'] = rtrim($this->conf['api_server'], '/');
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function renderNoCaptcha() {
+		return '<div class="g-recaptcha" data-sitekey="' . $this->conf['public_key'] . '"></div>';
+	}
+
+	/**
+	 * @param $error
+	 * @return string
+	 */
+	protected function renderReCaptcha($error) {
 		// were any errors given in the last query?
 		$err_parameter = '';
 		if ($error) {
-			$err__parameter = '&amp;error=' . $error;
+			$err_parameter = '&amp;error=' . $error;
 		}
 
 		###############################
@@ -76,46 +102,46 @@ class tx_jmrecaptcha extends tslib_pibase {
 		);
 
 		// Theme
-		if( !empty($conf['theme']) ) {
-			$recaptchaOptions['theme'] = self::jsQuote( $conf['theme'] );
+		if (!empty($this->conf['theme'])) {
+			$recaptchaOptions['theme'] = self::jsQuote($this->conf['theme']);
 		}
 
 		// TabIndex
-		if( !empty($conf['tabindex']) ) {
-			$recaptchaOptions['tabindex'] = self::jsQuote( $conf['tabindex'] );
+		if (!empty($this->conf['tabindex'])) {
+			$recaptchaOptions['tabindex'] = self::jsQuote($this->conf['tabindex']);
 		}
 
 		// TabIndex
-		if( !empty($conf['custom_theme_widget']) ) {
-			$recaptchaOptions['custom_theme_widget'] = self::jsQuote( $conf['custom_theme_widget'] );
+		if (!empty($this->conf['custom_theme_widget'])) {
+			$recaptchaOptions['custom_theme_widget'] = self::jsQuote($this->conf['custom_theme_widget']);
 		}
 
 		// Language detection
-		if( !empty($conf['lang']) ) {
+		if (!empty($this->conf['lang'])) {
 			// language from plugin configuration
-			$recaptchaOptions['lang'] = self::jsQuote( $conf['lang'] );
-		} elseif( !empty($TSFE->tmpl->setup['config.']['language']) ) {
+			$recaptchaOptions['lang'] = self::jsQuote($this->conf['lang']);
+		} elseif (!empty($this->typoscriptFrontendController->tmpl->setup['config.']['language'])) {
 			// automatic language detection (TYPO3 settings)
-			$recaptchaOptions['lang'] = self::jsQuote( $TSFE->tmpl->setup['config.']['language'] );
+			$recaptchaOptions['lang'] = self::jsQuote($this->typoscriptFrontendController->tmpl->setup['config.']['language']);
 		}
 
 		// Custom translations
 		$customTranslations = array();
 
-		if( !empty($conf['custom_translations.']) ) {
-			foreach($conf['custom_translations.'] as $translationKey => $translationValue) {
-				$customTranslations[] = $translationKey.' : '.self::jsQuote( $translationValue );
+		if (!empty($this->conf['custom_translations.'])) {
+			foreach ($this->conf['custom_translations.'] as $translationKey => $translationValue) {
+				$customTranslations[] = $translationKey . ' : ' . self::jsQuote($translationValue);
 			}
 		}
-		if( !empty($customTranslations) ) {
-			$recaptchaOptions['custom_translations'] = ' { '.implode(',',$customTranslations).' } ';
+		if (!empty($customTranslations)) {
+			$recaptchaOptions['custom_translations'] = ' { ' . implode(',', $customTranslations) . ' } ';
 		}
 
 
 		// Build option string
 		$recaptchaOptionsTmp = array();
-		foreach($recaptchaOptions as $optionKey => $optionValue) {
-			$recaptchaOptionsTmp[] = $optionKey.' : '.$optionValue;
+		foreach ($recaptchaOptions as $optionKey => $optionValue) {
+			$recaptchaOptionsTmp[] = $optionKey . ' : ' . $optionValue;
 		}
 		$recaptchaOptions = implode(', ', $recaptchaOptionsTmp);
 
@@ -123,63 +149,67 @@ class tx_jmrecaptcha extends tslib_pibase {
 		###############################
 		# reCaptcha HTML
 		###############################
-		$content = '<script type="text/javascript">var RecaptchaOptions = { '.$recaptchaOptions.' };</script>';
-		$content .= '<script type="text/javascript" src="'.htmlspecialchars($server.'/challenge?k='.$key.$err__parameter).'"></script>';
-		$content .= '<noscript><iframe class="recaptcha-noscript-frame" src="'.htmlspecialchars($server.'/noscript?k='.$key).'" height="300" width="500" frameborder="0"></iframe><br /><p>'.$this->pi_getLL('enter_code_here').'</p><textarea class="recaptcha-noscript-textarea" name="recaptcha_challenge_field" rows="3" cols="40"></textarea><input type="hidden" name="recaptcha_response_field" value="manual_challenge" /></noscript>';
+		$content = '<script type="text/javascript">var RecaptchaOptions = { ' . $recaptchaOptions . ' };</script>';
+		$content .= '<script type="text/javascript" src="' . htmlspecialchars($this->conf['server'] . '/challenge?k=' . $this->conf['public_key'] . $err_parameter) . '"></script>';
+		$content .= '<noscript><iframe class="recaptcha-noscript-frame" src="' . htmlspecialchars($this->conf['server'] . '/noscript?k=' . $this->conf['public_key']) . '" height="300" width="500" frameborder="0"></iframe><br /><p>' . $this->pi_getLL('enter_code_here') . '</p><textarea class="recaptcha-noscript-textarea" name="recaptcha_challenge_field" rows="3" cols="40"></textarea><input type="hidden" name="recaptcha_response_field" value="manual_challenge" /></noscript>';
 		return $content;
 	}
 
 	/**
 	 * Quote js-param-value
 	 *
-	 * @param 	string	$value	Value
-	 * @return	string			Quoted value
+	 * @param  string $value Value
+	 * @return  string      Quoted value
 	 */
 	protected static function jsQuote($value) {
-		return '\''.addslashes( (string)$value ).'\'';
+		return '\'' . addslashes((string)$value) . '\'';
 	}
 
 	/**
 	 * Validate reCAPTCHA challenge/response
 	 *
-	 * @return	array		Array with verified- (boolean) and error-code (string)
+	 * @return  array    Array with verified- (boolean) and error-code (string)
 	 */
 	public function validateReCaptcha() {
-		global $TSFE;
-		// save plugin configuration inside a local array
-		$conf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_jmrecaptcha.'];
+		$this->initConfiguration();
 
-		// read out challenge, answer and remote_addr
-		$data = array(
-			'remoteip'		=> $_SERVER['REMOTE_ADDR'],
-			'challenge'		=> trim( (string)t3lib_div::_GP('recaptcha_challenge_field') ),
-			'response'		=> trim( (string)t3lib_div::_GP('recaptcha_response_field') ),
-			'privatekey'	=> $conf['private_key'],
-		);
-
-		// first discard useless input
-		if( empty($data['challenge']) || empty($data['response']) ) {
-			return false;
-		}
-
-		if( $this->getExtConf('oldFetchMethod', false) ) {
-			$result = $this->query_verification_server_old($data);
+		if ($this->conf['captcha_type'] === 'nocaptcha') {
+			$data = array(
+				'remoteip' => $_SERVER['REMOTE_ADDR'],
+				'response' => trim((string)\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('g-recaptcha-response')),
+				'secret' => $this->conf['private_key'],
+			);
+			if (empty($data['response'])) {
+				return FALSE;
+			}
+		} elseif ($this->conf['captcha_type'] === 'recaptcha') {
+			$data = array(
+				'remoteip' => $_SERVER['REMOTE_ADDR'],
+				'challenge' => trim((string)\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('recaptcha_challenge_field')),
+				'response' => trim((string)\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('recaptcha_response_field')),
+				'privatekey' => $this->conf['private_key'],
+			);
+			if (empty($data['challenge']) || empty($data['response'])) {
+				return FALSE;
+			}
 		} else {
-			$result = $this->query_verification_server($data);
+			throw new \Exception('Invalid captcha_type', 1430388724);
 		}
 
-		if(!$result) {
+		$result = $this->query_verification_server($data);
+
+		if (!$result) {
 			return false;
 		}
 
 		// depending on the result, return appropriate status object
 		$ret = array();
-		if( strtolower($result[0]) == 'true' ) {
-			$ret['verified']	= true;
-			$ret['error']		= NULL;
+		if ($result[0]) {
+			$ret['verified'] = true;
+			$ret['error'] = NULL;
 		} else {
-			$ret['verified']	= false;
-			$ret['error']		= $result[1];
+			$ret['verified'] = false;
+			$ret['error'] = $result[1];
 		}
 
 		return $ret;
@@ -187,91 +217,52 @@ class tx_jmrecaptcha extends tslib_pibase {
 
 	/**
 	 * Query reCAPTCHA server for captcha-verification
-	 *
-	 * @param	array		Data
-	 * @return	array		Array with verified- (boolean) and error-code (string)
+	 * @param array $data
+	 * @return array Array with verified- (boolean) and error-code (string)
 	 */
 	protected function query_verification_server($data) {
-		global $TSFE;
-
-		// save plugin configuration inside a local array
-		$conf   = $TSFE->tmpl->setup['plugin.']['tx_jmrecaptcha.'];
 
 		// find first occurence of '//' inside server string
-		$verifyServerInfo = @parse_url( $conf['verify_server'] );
+		$verifyServerInfo = @parse_url($this->conf['verify_server']);
 
-		if( empty($verifyServerInfo) ) {
+		if (empty($verifyServerInfo)) {
 			return array(false, 'recaptcha-not-reachable');
 		}
 
-		$paramStr = t3lib_div::implodeArrayForUrl('', $data);
-		$ret = t3lib_div::getURL($conf['verify_server'] . '?' . $paramStr);
-
-		return t3lib_div::trimExplode("\n", $ret);
-	}
-
-	/**
-	 * Query reCAPTCHA server for captcha-verification
-	 *
-	 * @param	array		Data
-	 * @return	array		Array with verified- (boolean) and error-code (string)
-	 */
-	protected function query_verification_server_old($data) {
-		global $TSFE;
-
-		// save plugin configuration inside a local array
-		$conf	= $TSFE->tmpl->setup['plugin.']['tx_jmrecaptcha.'];
-
-		// find first occurence of '//' inside server string
-		$verifyServerInfo = @parse_url( $conf['verify_server'] );
-
-		if( empty($verifyServerInfo) ) {
-			return array(false, 'recaptcha-not-reachable');
+		if ($this->conf['captcha_type'] === 'nocaptcha') {
+			$ch = curl_init();
+			curl_setopt($ch,CURLOPT_URL, $this->conf['verify_server']);
+			curl_setopt($ch,CURLOPT_POST, count($data));
+			curl_setopt($ch,CURLOPT_POSTFIELDS, \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('', $data));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$response = json_decode(curl_exec($ch), TRUE);
+			$result = array(
+				$response['success'],
+				isset($response['error-codes']) ? $response['error-codes'][0] : ''
+			);
+			curl_close($ch);
+		} elseif ($this->conf['captcha_type'] === 'recaptcha') {
+			$paramStr = \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('', $data);
+			$ret = \TYPO3\CMS\Core\Utility\GeneralUtility::getURL($this->conf['verify_server'] . '?' . $paramStr);
+			$result = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode("\n", $ret);
+			$result[0] = $result[0] == 'true';
+		} else {
+			throw new \Exception('Invalid captcha_type', 1430388724);
 		}
-
-		// wrap parameters
-		$req = $this->qsencode($data);
-
-		// compose HTTP request
-		$http_request  = "POST ".$verifyServerInfo['path']." HTTP/1.0\r\n";
-		$http_request .= "Host: ".$verifyServerInfo['host']."\r\n";
-		$http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
-		$http_request .= "Content-Length: " . strlen($req) . "\r\n";
-		$http_request .= "User-Agent: TYPO3/PHP\r\n";
-		$http_request .= "\r\n";
-		$http_request .= $req;
-
-		// open socket
-		if( !$fs = @fsockopen($verifyServerInfo['host'], $verifyServerInfo['port'], $errno, $errstr, 10) ) {
-			return array(false, 'recaptcha-not-reachable');
-		}
-
-		// write request
-		fwrite($fs, $http_request);
-
-		// get response
-		$response = '';
-		while ( !feof($fs) ) {
-			$response .= fgets($fs, 1160); // One TCP-IP packet
-		}
-		fclose($fs);
-		$response = explode("\r\n\r\n", $response, 2);
-
-		// return it
-		return explode("\n", $response[1]);
+		return $result;
 	}
 
 	/**
 	 * QueryServer encode values
 	 *
-	 * @param	array		Data
-	 * @return	array		Encoded data
+	 * @param array $data
+	 * @return array Encoded data
 	 */
-	protected static function qsencode ($data) {
+	protected static function qsencode($data) {
 		$ret = "";
 
-		foreach( $data as $key => $value ) {
-			$ret .= $key . '=' . urlencode( stripslashes($value) ) . '&';
+		foreach ($data as $key => $value) {
+			$ret .= $key . '=' . urlencode(stripslashes($value)) . '&';
 		}
 
 		$ret = rtrim($ret, '&');
@@ -282,23 +273,23 @@ class tx_jmrecaptcha extends tslib_pibase {
 	/**
 	 * Get extension configuration (by name)
 	 *
-	 * @param	string	$name			Configuration settings name
-	 * @param	mixed	$defaultValue	Default value (if configuration doesn't exists)
-	 * @return	mixed
+	 * @param  string $name Configuration settings name
+	 * @param  mixed $defaultValue Default value (if configuration doesn't exists)
+	 * @return  mixed
 	 */
 	protected function getExtConf($name, $defaultValue = NULL) {
 		global $TYPO3_CONF_VARS;
 
-		if($this->extConf === NULL) {
+		if ($this->extConf === NULL) {
 			// Load ext conf
 			$this->extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['jm_recaptcha']);
-			if(!is_array($this->extConf)) {
+			if (!is_array($this->extConf)) {
 				$this->extConf = array();
 			}
 		}
 
 		$ret = $defaultValue;
-		if(!empty($this->extConf[$name])) {
+		if (!empty($this->extConf[$name])) {
 			$ret = $this->extConf[$name];
 		}
 
@@ -306,9 +297,3 @@ class tx_jmrecaptcha extends tslib_pibase {
 	}
 
 }
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jm_recaptcha/class.tx_jmrecaptcha.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/jm_recaptcha/class.tx_jmrecaptcha.php']);
-}
-
-?>
